fe2bbae78c07ec749a35e38a9aa8a1a122be1e36
petarkapec <petar.kapec@fer.hr>
2024-10-30 20:00:36 +0100

    Petar Kapec - inicjalno pokretanje backenda-SpringBoot
    Korištenje packet managera Maven
    inicijaziran projekt pomoću https://start.spring.io/
    
    //ubacivanje osnovnih REST dependencies (Java Spring boot web)
    
    inicijalizacija react frontenda
    
    //instaliranje osnove axios bibilioteke koja omogućuje baratanje s endpointovima (REST)
    
    //omogućavanje CORS-a tako da mogu komunicirati frontend i backend (u trenutnoj inačici dopušteno je iskljucivo sa adrese localhost:3000
    no mozda cemo dopustiti sa svih portova kasnije)
    
    //instaliran react router DOM
    
    //kreirana osnovni mock-up login samo se provjerava jesu li sifra i user admin admin
    
    kreirane komponente Login i dashboard u reactu

---
+++ IzvorniKod/jollybringer/src/main/java/hr/unizg/fer/jollybringer/JollyBringerApplication.java
@@ -0,0 +1,13 @@
+package hr.unizg.fer.jollybringer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JollyBringerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JollyBringerApplication.class, args);
+	}
+
+}
---
+++ IzvorniKod/jollybringer/src/test/java/hr/unizg/fer/jollybringer/JollyBringerApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.unizg.fer.jollybringer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JollyBringerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
