e7fc437f2a7deaf4c8f0022e6c356564be317737
brunojurakic <bruno.jurakic@fer.unizg.hr>
2024-10-30 20:23:09 +0100

    create login page

--- IzvorniKod/frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-const App = () => {
-  return (
-    <div className='wrapper'>
-      App
-    </div>
-  )
-}
-
-export default App
---
+++ IzvorniKod/frontend/src/components/App.jsx
@@ -0,0 +1,12 @@
+import Login from "./Login.jsx";
+import '../styles/App.css'
+
+const App = () => {
+  return (
+    <>
+      <Login/>
+    </>
+  )
+}
+
+export default App
