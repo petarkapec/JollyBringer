f912e25ad14d8582cfdadb24dc4664adc251b495
petarkapec <petar.kapec@fer.hr>
2024-11-15 18:47:46 +0100

    update

--- IzvorniKod/jollybringer-frontend/vite.config.js
+++ IzvorniKod/jollybringer-frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api/": {
+        target: REACT_APP_BACKEND_URL,
+        changeOrigin: true
+      },
+    },
+  },
 })
