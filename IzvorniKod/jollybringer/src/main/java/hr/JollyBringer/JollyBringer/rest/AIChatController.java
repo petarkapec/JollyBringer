49a6c221dbd9c7cf6f06629a5a31e04cf42d4c31
Ante Mrkonjić <ante.mrkonjic@fer.unizg.hr>
2025-01-12 22:40:53 +0100

    Dodana ai funkcionalnost i kostur chata

--- IzvorniKod/jollybringer/src/main/java/hr/JollyBringer/JollyBringer/rest/AIChatController.java
+++ IzvorniKod/jollybringer/src/main/java/hr/JollyBringer/JollyBringer/rest/AIChatController.java
@@ -3,7 +3,6 @@ package hr.JollyBringer.JollyBringer.rest;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.mistralai.MistralAiChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,23 +15,24 @@ import reactor.core.publisher.Flux;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/")
 public class AIChatController {
 
-    private final MistralAiChatModel chatModel;
+    private final OpenAiChatModel chatModel;
 
     @Autowired
-    public AIChatController(MistralAiChatModel chatModel) {
+    public AIChatController(OpenAiChatModel chatModel) {
         this.chatModel = chatModel;
     }
 
     @GetMapping("/ai/generate")
-    public Map<String,String> generate(@RequestParam(value = "message", defaultValue = "Tell me how to finish my project on time") String message) {
+    public Map<String,String> generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
         return Map.of("generation", this.chatModel.call(message));
     }
 
     @GetMapping("/ai/generateStream")
-    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me how to finish my project on time") String message) {
-        var prompt = new Prompt(new UserMessage(message));
+    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
         return this.chatModel.stream(prompt);
     }
 }
