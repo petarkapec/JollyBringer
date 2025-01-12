284b49feb0a03b4db1f37276a74274e400c40ae5
Ante Mrkonjić <ante.mrkonjic@fer.unizg.hr>
2025-01-12 23:01:53 +0100

    Izbačen stari agent iz koda

--- IzvorniKod/jollybringer/src/main/java/hr/JollyBringer/JollyBringer/rest/AIChatController.java
+++ IzvorniKod/jollybringer/src/main/java/hr/JollyBringer/JollyBringer/rest/AIChatController.java
@@ -4,7 +4,7 @@ import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.mistralai.MistralAiChatModel;
-import org.springframework.ai.openai.OpenAiChatModel;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
