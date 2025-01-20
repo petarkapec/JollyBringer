93b241512649928a739906fb44cef26d2d31180a
petarkapec <petar.kapec@fer.hr>
2025-01-20 12:53:45 +0100

    mini2

--- IzvorniKod/jollybringer-frontend/vite.config.js
+++ IzvorniKod/jollybringer-frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { NodePolyfills } from 'vite-plugin-node-polyfills';
+import NodePolyfills from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   plugins: [
