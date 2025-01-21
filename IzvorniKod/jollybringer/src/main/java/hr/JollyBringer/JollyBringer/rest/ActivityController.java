88148f74e9c3297eac7fa00e67ef0c24cf386630
Ante Mrkonjić <ante.mrkonjic@fer.unizg.hr>
2025-01-21 22:30:12 +0100

    dodane rute za fetchanje aktivnosti iz dashboarda i njihovo stvaranje

--- IzvorniKod/jollybringer/src/main/java/hr/JollyBringer/JollyBringer/rest/ActivityController.java
+++ IzvorniKod/jollybringer/src/main/java/hr/JollyBringer/JollyBringer/rest/ActivityController.java
@@ -2,22 +2,17 @@ package hr.JollyBringer.JollyBringer.rest;
 
 import hr.JollyBringer.JollyBringer.domain.Activity;
 import hr.JollyBringer.JollyBringer.domain.Feedback;
-import hr.JollyBringer.JollyBringer.domain.Participant;
 import hr.JollyBringer.JollyBringer.service.ActivityService;
 import hr.JollyBringer.JollyBringer.service.FeedbackService;
-import hr.JollyBringer.JollyBringer.service.ParticipantGroupService;
 import hr.JollyBringer.JollyBringer.service.ParticipantService;
 import hr.JollyBringer.JollyBringer.service.impl.ActivityServiceJPA;
 import hr.JollyBringer.JollyBringer.service.impl.FeedbackServiceJPA;
-import hr.JollyBringer.JollyBringer.service.impl.ParticipantGroupServiceJPA;
 import hr.JollyBringer.JollyBringer.service.impl.ParticipantServiceJpa;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -47,12 +42,15 @@ public class ActivityController {
         return ResponseEntity.ok(feedbacks);
     }
 
+    //TODO beskorisna funkcija?
     @PostMapping("")
     //@Secured("ROLE_ADMIN")
     public ResponseEntity<Activity> createActivity(@RequestBody Activity activity){
         Activity saved = activityService.createActivity(activity);
         return ResponseEntity.created(URI.create("/activities/" + saved.getId())).body(saved);
     }
+
+
 
     @PostMapping("/{activityId}/feedback")
     public ResponseEntity<Feedback> addFeedback(
